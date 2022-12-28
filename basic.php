c3b9e64814e52fb90eabe8a756f9c797707e1fe7
unknown <cosmicindustries@protonmail.com>
2022-12-27 22:32:45 -0500

    Fix Links

--- basic.php
+++ basic.php
@@ -7,31 +7,30 @@
     <header>
       <h1 class="neon">Penetration Testing Portfolio</h1>
     </header>
-    <nav>
-	</br>
-      <ul>
-        <li><a href="/about.php">About</a></li>
-        <li><a href="/services.php">Services</a></li>
-        <li><a href="/projects.php">Projects</a></li>
-        <li><a href="/contact.php">Contact</a></li>
-      </ul>
-    </nav>
+ <nav>
+        <ul>
+          <li><a href="/about.html">About</a></li>
+          <li><a href="/services.html">Services</a></li>
+          <li><a href="/projects.html">Projects</a></li>
+          <li><a href="/contact.html">Contact</a></li>
+        </ul>
+      </nav>
     <main>
       <section class="card holographic" id="about">
-        <a href="/about.php"><h2>About</h2></a>
+        <a href="/about.html"><h2>About</h2></a>
 		<p>
           As an individual with a background in computer science and a strong passion for cybersecurity, I have always been drawn to the world of penetration testing. Growing up, I spent countless hours tinkering with computers and learning about different security protocols and vulnerabilities. As I progressed through my education, I became more and more interested in the concept of ethical hacking and the role it plays in protecting organizations from cyber threats. I have always been driven by a desire to use my knowledge and skills to make a positive impact in the world, and I believe that pursuing a career in penetration testing will allow me to do just that. I am excited to continue learning and growing as a professional in this field, and I am confident that my dedication and hard work will enable me to succeed in any challenge that comes my way.
         </p>
       </section>
       <section class="card holographic" id="services">
-        <a href="/services.php"><h2>Services</h2></a>
+        <a href="/services.html"><h2>Services</h2></a>
         <ul>
           <li>As a professional in the field of penetration testing, I am skilled in a variety of services that can help organizations protect their networks and systems from cyber threats. Some of the services I can offer include network penetration testing, which involves identifying vulnerabilities in a company's network infrastructure and providing recommendations for improvement. I am also proficient in web application penetration testing, which involves identifying vulnerabilities in a company's web-based applications and providing recommendations for improvement. In addition, I can offer mobile application penetration testing, which involves identifying vulnerabilities in a company's mobile applications and providing recommendations for improvement. Overall, my goal is to provide comprehensive and thorough testing services that can help organizations strengthen their defenses against cyber attacks and keep their sensitive data safe.
 </li>
         </ul>
       </section>
       <section class="card holographic" id="projects">
-        <a href="/projects.php"><h2>Projects</a></h2>
+        <a href="/projects.html"><h2>Projects</a></h2>
         <ol>
           <li>    Network Penetration Testing for a Healthcare Organization: In this project, I used a variety of tools and techniques, including Nmap and Metasploit, to identify vulnerabilities in the healthcare organization's network infrastructure. I provided recommendations for improving security protocols and hardening the network against potential attacks.
 
@@ -46,14 +45,14 @@
       </section>
 		<section class="card holographic">
           <div id="contact">
-          <a href="/contact.php"><h2> Contact</h2></a>
+          <a href="/contact.html"><h2> Contact</h2></a>
             <p><a href="https://github.com/CosmicIndustries"><h3><li>Github</h3> </a></p></li>
           </div>
         </section>
     </main>
-	<!-- FIXME: Note that to use PHP in an HTML document, you will need to save the file with a .php extension and have a PHP interpreter installed on the server that is hosting the website. You will also need to make sure that the PHP code is properly enclosed within <?php and ?> tags. -->
+	<!-- FIXME: Note that to use html in an HTML document, you will need to save the file with a .html extension and have a html interpreter installed on the server that is hosting the website. You will also need to make sure that the html code is properly enclosed within <?html and ?> tags. -->
     <footer>
-      <p>Copyright© <script>document.write(new Date().getFullYear());</script> <noscript><?php echo date("Y"); ?></noscript> Cosmic Industries LLC</p>
+      <p>Copyright© <script>document.write(new Date().getFullYear());</script> <noscript><?html echo date("Y"); ?></noscript> Cosmic Industries LLC</p>
     </footer>
   </body>
 </html>
